36464cee9b6b58cedb8d3d0f1d62fa814bca6818
somesh-ghaturle <someshghaturle@gmail.com>
2026-07-31 17:06:34 -0400

    version 1

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">ghost AI</h1>
+      </div>
+    </div>
+  );
+}
